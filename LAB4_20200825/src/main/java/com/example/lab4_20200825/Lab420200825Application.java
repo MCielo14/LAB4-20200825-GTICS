a0d10395a3ea1fb150a89c5787788a3c3c8fb0e1
= <=>
2023-09-07 20:42:19 -0500

    Inicio del lab

---
+++ LAB4_20200825/src/main/java/com/example/lab4_20200825/Lab420200825Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20200825;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420200825Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab420200825Application.class, args);
+    }
+
+}
---
+++ LAB4_20200825/src/test/java/com/example/lab4_20200825/Lab420200825ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20200825;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420200825ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
